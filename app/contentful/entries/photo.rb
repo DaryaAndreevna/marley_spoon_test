64ae3e84d3d907d669180c25ad01074e512e2b2e
Darya Karazayeva <karazayeva.darya@gmail.com>
2021-10-18 18:19:08 +0300

    Clean up

--- app/contentful/entries/photo.rb
+++ app/contentful/entries/photo.rb
@@ -2,8 +2,24 @@ module Entries
   class Photo < Base
     fields :file
 
-    def url
-      @url ||= "https:#{file.url}"
+    SIZES = %i[thumb medium].freeze
+
+    def url(size = nil)
+      case size
+      when :thumb then version(300)
+      when :medium then version(500)
+      else origin_url
+      end
+    end
+
+    private
+
+    def version(width)
+      "#{origin_url}?#{{ w: width }.to_query}"
+    end
+
+    def origin_url
+      @origin_url ||= "https:#{file.url}"
     end
   end
 end
--- app/helpers/application_helper.rb
+++
@@ -1,5 +0,0 @@
-module ApplicationHelper
-  def contentful_image(url, params = {})
-    "https:#{url}?#{params.to_query}"
-  end
-end
